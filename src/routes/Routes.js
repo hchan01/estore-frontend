be3e04f4ef1db5baf03695d3823f38cb4a7c852e
Harry <harry@hchan.io>
2020-11-18 23:22:36 +0000

    Wrapped routes in a 'main' element

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -17,22 +17,24 @@ import {
 } from '../screens';
 
 const Routes = () => (
-    <ScrollToTop>
-        <Switch>
-            <Route exact path="/" component={HomeScreen} />
-            <Route exact path="/sign-in" component={SignInScreen} />
-            <Route exact path="/register" component={RegisterScreen} />
-            <Route exact path="/category/:categoryId" component={ProductListingScreen} />
-            <Route exact path="/products/:productId" component={ProductScreen} />
-            <Route exact path="/cart" component={CartScreen} />
-            <Route exact path="/checkout/delivery" component={DeliveryScreen} />
-            <Route exact path="/checkout/payment" component={PaymentScreen} />
-            <Route exact path="/checkout/order/:orderId" component={OrderScreen} />
-            <Route exact path="/privacy-policy" component={PrivacyPolicyScreen} />
-            <Route exact path="/terms-and-conditions" component={TermsAndConditionsScreen} />
-            <Route component={PageNotFoundScreen} status={404} />
-        </Switch>
-    </ScrollToTop>
+    <main>
+        <ScrollToTop>
+            <Switch>
+                <Route exact path="/" component={HomeScreen} />
+                <Route exact path="/sign-in" component={SignInScreen} />
+                <Route exact path="/register" component={RegisterScreen} />
+                <Route exact path="/category/:categoryId" component={ProductListingScreen} />
+                <Route exact path="/products/:productId" component={ProductScreen} />
+                <Route exact path="/cart" component={CartScreen} />
+                <Route exact path="/checkout/delivery" component={DeliveryScreen} />
+                <Route exact path="/checkout/payment" component={PaymentScreen} />
+                <Route exact path="/checkout/order/:orderId" component={OrderScreen} />
+                <Route exact path="/privacy-policy" component={PrivacyPolicyScreen} />
+                <Route exact path="/terms-and-conditions" component={TermsAndConditionsScreen} />
+                <Route component={PageNotFoundScreen} status={404} />
+            </Switch>
+        </ScrollToTop>
+    </main>
 )
 
 export default Routes;
